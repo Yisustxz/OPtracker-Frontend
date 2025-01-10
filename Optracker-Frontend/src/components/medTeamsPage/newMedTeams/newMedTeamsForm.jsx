49970807f86c37a03bb3f37ab54973c19fa9ce47
Yisustxz <jesusoortiz2410@gmail.com>
2025-01-09 22:14:11 -0400

    actualizado estilados y agregado educacion

--- Optracker-Frontend/src/components/medTeamsPage/newMedTeams/newMedTeamsForm.jsx
+++ Optracker-Frontend/src/components/medTeamsPage/newMedTeams/newMedTeamsForm.jsx
@@ -1,103 +1,139 @@
 function NewMedTeamForm({ formData, setFormData, addEducationField }) {
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({ ...formData, [name]: value });
-    };
-  
-    return (
-      <form className="space-y-6">
-        <div className="grid grid-cols-2 gap-4 text-left">
-          {/* Nombre Completo */}
-          <div>
-            <label className="block font-medium mb-2">Nombre Completo</label>
-            <input
-              type="text"
-              name="nombre"
-              value={formData.nombre}
-              onChange={handleChange}
-              className="border w-full px-4 py-2 rounded-xl mb-4"
-              placeholder="Nombre"
-              required
-            />
-          </div>
-  
-          {/* Cédula */}
-          <div>
-            <label className="block font-medium mb-2">Cédula</label>
-            <input
-              type="text"
-              name="cedula"
-              value={formData.cedula}
-              onChange={handleChange}
-              className="border w-full px-4 py-2 rounded-xl mb-4"
-              placeholder="Cédula"
-              required
-            />
-          </div>
-  
-          {/* Correo */}
-          <div>
-            <label className="block font-medium mb-2">Correo</label>
-            <input
-              type="email"
-              name="correo"
-              value={formData.correo}
-              onChange={handleChange}
-              className="border w-full px-4 py-2 rounded-xl mb-4"
-              placeholder="Correo"
-              required
-            />
-          </div>
-  
-          {/* Especialidad */}
-          <div>
-            <label className="block font-medium mb-2">Especialidad</label>
-            <input
-              type="text"
-              name="especialidad"
-              value={formData.especialidad}
-              onChange={handleChange}
-              className="border w-full px-4 py-2 rounded-xl mb-4"
-              placeholder="Especialidad"
-              required
-            />
-          </div>
-  
-          {/* Número de Licencia Médica */}
-          <div>
-            <label className="block font-medium mb-2">
-              Número de Licencia Médica
-            </label>
-            <input
-              type="text"
-              name="licencia"
-              value={formData.licencia}
-              onChange={handleChange}
-              className="border w-full px-4 py-2 rounded-xl mb-4"
-              placeholder="Número de licencia"
-              required
-            />
-          </div>
-  
-          {/* Número DEA */}
-          <div>
-            <label className="block font-medium mb-2">Número DEA</label>
-            <input
-              type="text"
-              name="numeroDEA"
-              value={formData.numeroDEA}
-              onChange={handleChange}
-              className="border w-full px-4 py-2 rounded-xl"
-              placeholder="Número DEA"
-              required
-            />
-          </div>
-        </div>
-  
-        
-      </form>
-    );
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
   }
-  
-  export default NewMedTeamForm;
-  
+
+  const handleEducationChange = (index, value) => {
+    const updatedEducation = [...formData.educacion]
+    updatedEducation[index] = value
+    setFormData({ ...formData, educacion: updatedEducation })
+  }
+
+  return (
+    <form className='space-y-6'>
+      <div className='grid grid-cols-2 gap-4 text-left'>
+        {/* Nombre Completo */}
+        <div>
+          <label className='block font-medium mb-2'>Nombre Completo</label>
+          <input
+            type='text'
+            name='nombre'
+            value={formData.nombre}
+            onChange={handleChange}
+            className='border w-full px-4 py-2 rounded-xl mb-4'
+            placeholder='Nombre'
+            required
+          />
+        </div>
+
+        {/* Cédula */}
+        <div>
+          <label className='block font-medium mb-2'>Cédula</label>
+          <input
+            type='text'
+            name='cedula'
+            value={formData.cedula}
+            onChange={handleChange}
+            className='border w-full px-4 py-2 rounded-xl mb-4'
+            placeholder='Cédula'
+            required
+          />
+        </div>
+
+        {/* Correo */}
+        <div>
+          <label className='block font-medium mb-2'>Correo</label>
+          <input
+            type='email'
+            name='correo'
+            value={formData.correo}
+            onChange={handleChange}
+            className='border w-full px-4 py-2 rounded-xl mb-4'
+            placeholder='Correo'
+            required
+          />
+        </div>
+
+        {/* Especialidad */}
+        <div>
+          <label className='block font-medium mb-2'>Especialidad</label>
+          <input
+            type='text'
+            name='especialidad'
+            value={formData.especialidad}
+            onChange={handleChange}
+            className='border w-full px-4 py-2 rounded-xl mb-4'
+            placeholder='Especialidad'
+            required
+          />
+        </div>
+
+        {/* Número de Licencia Médica */}
+        <div>
+          <label className='block font-medium mb-2'>
+            Número de Licencia Médica
+          </label>
+          <input
+            type='text'
+            name='licencia'
+            value={formData.licencia}
+            onChange={handleChange}
+            className='border w-full px-4 py-2 rounded-xl mb-4'
+            placeholder='Número de licencia'
+            required
+          />
+        </div>
+
+        {/* Número DEA */}
+        <div>
+          <label className='block font-medium mb-2'>Número DEA</label>
+          <input
+            type='text'
+            name='numeroDEA'
+            value={formData.numeroDEA}
+            onChange={handleChange}
+            className='border w-full px-4 py-2 rounded-xl'
+            placeholder='Número DEA'
+            required
+          />
+        </div>
+      </div>
+
+      {/* Educación */}
+      <div className='mt-6 flex flex-col items-center'>
+        <label className='block font-medium mb-2'>Educación</label>
+        {formData.educacion?.map((edu, index) => (
+          <div key={index} className='flex justify-center gap-4 mb-4'>
+            <select
+              value={edu}
+              onChange={(e) => handleEducationChange(index, e.target.value)}
+              className='border w-96 px-4 py-2 rounded-xl'
+            >
+              <option value='' disabled>
+                Selecciona un título académico
+              </option>
+              <option value='Bachiller'>Bachiller</option>
+              <option value='Técnico Superior Universitario'>
+                Técnico Superior Universitario
+              </option>
+              <option value='Licenciatura'>Licenciatura</option>
+              <option value='Ingeniería'>Ingeniería</option>
+              <option value='Maestría'>Maestría</option>
+              <option value='Doctorado'>Doctorado</option>
+            </select>
+          </div>
+        ))}
+        <button
+          type='button'
+          onClick={addEducationField}
+          className='flex items-center justify-center w-48 border px-4 py-2 rounded-xl mt-2'
+        >
+          + Agregar Educación
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default NewMedTeamForm
--- Optracker-Frontend/src/pages/newMedTeams.jsx
+++ Optracker-Frontend/src/pages/newMedTeams.jsx
@@ -1,34 +1,34 @@
-import Navigation from "@/components/ui/navigation";
-import NewMedTeamHeader from "@/components/medTeamsPage/newMedTeams/newMedTeamsHeader";
-import NewMedTeamForm from "@/components/medTeamsPage/newMedTeams/newMedTeamsForm";
-import { useState } from "react";
+import Navigation from '@/components/ui/navigation'
+import NewMedTeamHeader from '@/components/medTeamsPage/newMedTeams/newMedTeamsHeader'
+import NewMedTeamForm from '@/components/medTeamsPage/newMedTeams/newMedTeamsForm'
+import { useState } from 'react'
 
 function NewMedTeams() {
-  const [selectedType, setSelectedType] = useState("Doctor");
+  const [selectedType, setSelectedType] = useState('Doctor')
   const [formData, setFormData] = useState({
-    nombre: "",
-    cedula: "",
-    correo: "",
-    especialidad: "",
-    licencia: "",
-    numeroDEA: "",
-    educacion: [""],
-  });
+    nombre: '',
+    cedula: '',
+    correo: '',
+    especialidad: '',
+    licencia: '',
+    numeroDEA: '',
+    educacion: ['']
+  })
 
   const addEducationField = () => {
-    setFormData({ ...formData, educacion: [...formData.educacion, ""] });
-  };
+    setFormData({ ...formData, educacion: [...formData.educacion, ''] })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Datos enviados:", formData);
-  };
+    e.preventDefault()
+    console.log('Datos enviados:', formData)
+  }
 
   return (
-    <div className="flex flex-col pt-1.5 bg-white">
+    <div className='flex flex-col pt-24 bg-white'>
       <Navigation />
-      <div className="flex justify-center px-40 py-5 max-md:px-5">
-        <div className="w-full max-w-[960px]">
+      <div className='flex justify-center px-40 py-5 max-md:px-5'>
+        <div className='w-full max-w-[960px]'>
           {/* Encabezado */}
           <NewMedTeamHeader
             selectedType={selectedType}
@@ -43,10 +43,10 @@ function NewMedTeams() {
           />
 
           {/* Botón de Registro */}
-          <div className="mt-8">
+          <div className='mt-8'>
             <button
               onClick={handleSubmit}
-              className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 w-1/2 mt-7"
+              className='bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 w-1/2 mt-7'
             >
               Registrar Equipo Médico
             </button>
@@ -54,7 +54,7 @@ function NewMedTeams() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default NewMedTeams;
+export default NewMedTeams
