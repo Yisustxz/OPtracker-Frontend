a1424e17d647b11db293f89c7dd760a1f3e16aa1
Yisustxz <jesusoortiz2410@gmail.com>
2025-01-19 23:02:18 -0400

    cambios de estilo para ajustar a la pantalla

--- Optracker-Frontend/src/pages/Statistics.jsx
+++ Optracker-Frontend/src/pages/Statistics.jsx
@@ -3,21 +3,18 @@ import Navigation from '@/components/ui/navigation'
 import DashboardSummary from '@/components/StatisticsPage/DashboardSummary'
 import OperationsChart from '@/components/StatisticsPage/OperationsChart'
 
-
-
 export default function Statistics() {
-     
-    return (
-        <div>
-            <div className='mb-0'>
-                <Navigation/>
-            </div>
-            <main className="pt-4 space-y-6 pt-0">
-              <div className='top-0'>
-                <DashboardSummary />
-                <OperationsChart />
-               </div>
-            </main>
+  return (
+    <div>
+      <div className='mb-0'>
+        <Navigation />
+      </div>
+      <main className='pt-28 space-y-6'>
+        <div className='top-0'>
+          <DashboardSummary />
+          <OperationsChart />
         </div>
-    )
+      </main>
+    </div>
+  )
 }
